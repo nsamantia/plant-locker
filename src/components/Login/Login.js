736da1dfe4411f90618cf633661e30dbaeb2bbc9
namantia1@gmail.com <namantia1@gmail.com>
2020-12-30 14:02:56 -0500

    quick styling fix

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -27,7 +27,7 @@ const Login = (props) => {
             
             <div className="image-container">
                 <h1>Digital storage for your plants</h1>
-                <p>This app was created with the purpose of reducing plastic building up in landfills and spilling over into habitats. Gardening is a productive hobby and a great way to contribute to creating biodiversity in your yard. Unfortunately, there is a lot of plastic involved in gardening and I hope to eliminate plastic id tags by giving people the opportunity to store their plants information digitally.</p>
+                <p>This app was created with the purpose of reducing plastic building up in landfills and spilling over into habitats. Gardening is a productive hobby and a great way to contribute to creating biodiversity in your yard. Unfortunately, there is a lot of plastic involved in gardening and I hope to eliminate plastic id tags by giving people the opportunity to store their plant information digitally.</p>
             </div>
                 <div className="login-form-container">
                     <div><img src={logo}/></div>
