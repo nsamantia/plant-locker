e5884d1a02b557f8639a1f8a9eaa166b0082bf16
namantia1@gmail.com <namantia1@gmail.com>
2021-01-04 15:58:35 -0500

    media query for info page styling

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -16,10 +16,12 @@ const Info = () => {
                 <p>This app was created with the purpose of reducing plastic building up in landfills and spilling over into habitats. Gardening is a productive hobby and a great way to contribute to creating biodiversity in your yard. Unfortunately, there is a lot of plastic involved in gardening and I hope to eliminate plastic id tags by giving people the opportunity to store their plant information digitally.</p>
             
                 <div>
-                   <Link to="/Login"><button>Sign In</button></Link>
-                </div>
-                <div>
-                   <Link to="/register"><button>Register</button></Link>
+                    <div className="button-container">
+                        <Link to="/Login"><button>Sign In</button></Link>
+                    </div>
+                    <div>
+                        <Link to="/register"><button>Register</button></Link>
+                    </div>
                 </div>
 
         </div>
