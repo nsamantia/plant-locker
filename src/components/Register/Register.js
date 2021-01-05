6ead4a1d858a41e49b10c7cf7d63dd863d98a2e1
namantia1@gmail.com <namantia1@gmail.com>
2021-01-05 16:25:05 -0500

    users can now register new accounts

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -3,10 +3,20 @@ import axios from 'axios'
 import {Link} from 'react-router-dom'
 import '../../scss/main.scss'
 
-const Register = () => {
+const Register = (props) => {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+
+    const registerUser = () => {
+        axios.post('/api/auth/register', {username, password, email})
+            .then(res => {
+            props.history.push('/login');
+        }).catch(err => alert(err.message))
+    }
+
+
 
     return(
         <div className="login-container">
@@ -15,13 +25,16 @@ const Register = () => {
                     <div className="input-container">
                         <h1>Plant Locker</h1>
                         <div>
-                            <input type="text" name="username" placeholder="password" onChange={(e)=>setUsername(e.target.value)}/>
+                            <input type="text" name="username" placeholder="Username" onChange={(e)=>setUsername(e.target.value)}/>
                         </div>
                         <div>
-                            <input type="text" name="password" placeholder="username" onChange={(e)=>setPassword(e.target.value)}/>
+                            <input type="text" name="password" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                        </div>
+                        <div>
+                            <input type="text" name="email" placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
                         </div>
                         <div className="login-btn-container">
-                            <button className="login-button">Register</button>
+                            <button className="login-button" onClick={()=>registerUser()}>Register</button>
                         </div>
                         <div>
                             <Link to='/'><button>Back</button></Link>
