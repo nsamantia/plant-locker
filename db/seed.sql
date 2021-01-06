d7a46adf9a948247431a136ca5e2e23dff737449
namantia1@gmail.com <namantia1@gmail.com>
2021-01-06 16:51:33 -0500

    seed file update

--- db/seed.sql
+++ db/seed.sql
@@ -10,16 +10,16 @@ CREATE TABLE plants_pl(
 id SERIAL PRIMARY KEY,
 plant varchar(70) NOT NULL,
 plant_category_id int NOT NULL REFERENCES category_pl(category_id),
+user_id INT REFERENCES users_pl(id),
 plant_image text,
-plant_comment varchar(1000),
-user_id INT REFERENCES users_pl(id)
-
+plant_comment varchar(1000)
 )
 
 CREATE TABLE category_pl(
 category_id SERIAL PRIMARY KEY,
 category varchar(80) NOT NULL,
-user_id int REFERENCES users_pl(id)
+user_id int REFERENCES users_pl(id),
+category_image text
 )
 
 
@@ -27,5 +27,6 @@ CREATE TABLE category_second_pl(
 category_id SERIAL PRIMARY KEY,
 category varchar(80) NOT NULL,
 user_id int REFERENCES users_pl(id),
-category_one_id int REFERENCES category_pl(category_id)
+category_one_id int REFERENCES category_pl(category_id),
+category_image text
 )
