3f20adc03c452893f56662d95ce59aaea1ad8c21
namantia1@gmail.com <namantia1@gmail.com>
2021-01-29 12:03:27 -0500

    plant upload using a URL - will implement s3 after functionality and styling is finsihed

--- src/components/NewPlant/NewPlant.js
+++ src/components/NewPlant/NewPlant.js
@@ -4,10 +4,10 @@ import { connect } from "react-redux";
 import { getFolder } from "../../ducks/folderReducer";
 
 const NewPlant = (props) => {
-  const [selectedFile, setSelectedFile] = useState("");
+  const [link, setLink] = useState("");
   const [folder, setFolder] = useState(props.folder.folder);
 
-  useEffect(() => {}, [selectedFile]);
+  useEffect(() => {}, [link]);
 
   const uploadPlant = () => {
     axios.post(`/api/plants/new/${folder}`);
@@ -16,8 +16,13 @@ const NewPlant = (props) => {
   return (
     <div>
       <h1>Upload a New Plant</h1>
-      <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
-      {/* <img src={selectedFile} alt="uploaded plant" /> */}
+      <form>
+        <label>
+          Link:
+          <input type="text" onChange={(e) => setLink(e.target.value)} />
+        </label>
+      </form>
+      <img src={link} alt="" />
     </div>
   );
 };
