991c970c90b96b3be6ea2047bc16ac9fd72e73e6
namantia1@gmail.com <namantia1@gmail.com>
2021-03-12 16:33:37 -0500

    users can go into and display specific place info

--- src/components/InPlant/InPlant.js
+++ src/components/InPlant/InPlant.js
@@ -1,12 +1,35 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const InPlant = () => {
+const InPlant = (props) => {
+  const [plant, setPlant] = useState("");
+  const [image, setImage] = useState("");
+  const [comment, setComment] = useState("");
 
-  
+  useEffect(() => {
+    getPlant();
+    return () => {
+      setPlant([]);
+    };
+  }, []);
+
+  const getPlant = () => {
+    axios
+      .get(`/api/plants/plant/${props.match.params.id}`)
+      .then((res) => {
+        setPlant(res.data[0].plant);
+        setImage(res.data[0].plant_image);
+        setComment(res.data[0].plant_comment);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
     <div>
       <p>InPLant.js</p>
+      {plant}
+      <img src={image} alt="plant" />
+      {comment}
     </div>
   );
 };
