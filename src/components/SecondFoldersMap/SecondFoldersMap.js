79a7600bbdfcb614efc27d8cc28ac4cc860fbd38
namantia1@gmail.com <namantia1@gmail.com>
2021-08-10 15:09:48 -0400

    styling for second folders

--- src/components/SecondFoldersMap/SecondFoldersMap.js
+++ src/components/SecondFoldersMap/SecondFoldersMap.js
@@ -3,13 +3,14 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { getFolder } from "../../ducks/folderReducer";
+import "../../css/FoldersMap.css";
 
 const SecondFoldersMap = (props) => {
   const getFolderTwo = () => {
     props.history.push(`/InSecondFolder/${props.category_id}`);
     props.getFolder(props.category_id);
   };
-
+  // Deletes single folder
   const deleteFolder = () => {
     axios
       .delete(`/api/folder/second/delete/${props.category_id}`)
@@ -18,11 +19,15 @@ const SecondFoldersMap = (props) => {
   };
 
   return (
-    <div>
-      <button onClick={() => getFolderTwo()}>
-        <p>{props.category}</p>
+    <div className="folder-map-container">
+      <button className="folder-button" onClick={() => getFolderTwo()}>
+        <img className="folder-image" src={props.category_image} alt="" />
+        <p className="folder-card-type">Folder</p>
+        <p className="folder-category">{props.category}</p>
       </button>
-      <button onClick={() => deleteFolder()}>X</button>
+      <button className="folder-delete-button" onClick={() => deleteFolder()}>
+        X
+      </button>
     </div>
   );
 };
